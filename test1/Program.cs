eb718e34a4cee8ef7cf0c250e2800153cf8da520
Илья <ilacikoridze357@gmail.com>
2023-01-02 21:40:15 +0300

    Сортировка по убыванию элементов каждой строки двумерного массива.

---
+++ test1/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+Console.Clear();
+int inputNumber(string str)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+Console.WriteLine($"Введите размер массива m x n и диапазон случайных значений:");
+int m = inputNumber("Введите m: ");
+int n = inputNumber("Введите n: ");
+int rand = inputNumber("Введите диапазон: от 1 до ");
+
+int[,] matrix = new int[m, n];
+RandMatrix(matrix);
+PrintMatrix(matrix);
+
+Console.WriteLine($"\nОтсортированный массив: ");
+ResMatrix(matrix);
+PrintMatrix(matrix);
+
+void ResMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void RandMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(rand);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
